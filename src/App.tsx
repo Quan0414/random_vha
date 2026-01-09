cb921ab929a3787e813a2e455e5507345de30c0d
Quan0414 <quanthcstd@gmail.com>
2026-01-09 15:35:57 +0700

    Fix build and add deployment config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { DATABASE, CategoryData } from './data/options';
 import confetti from 'canvas-confetti';
 import { Heart, Sparkles, Wand2 } from 'lucide-react';
